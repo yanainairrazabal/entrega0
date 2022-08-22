6ae158ea8e4125ccb3bd6774aab52e5ea2aa2b40
yanainairrazabal <110423905+yanainairrazabal@users.noreply.github.com>
2022-08-21 22:48:47 -0300

    Entrega1
    Agregar pantalla login
    Agregada login js
    Agregada login productos

--- js/login.js
+++ js/login.js
@@ -2,27 +2,31 @@ let form = document.getElementById("form-login");
 let container_listing = document.getElementById("container-listing");
 let container_login = document.getElementById("container-login");
 
+function setAlert(msg){
+    document.getElementById("alert-container").innerHTML = `<div class='alert alert-danger alert-dismissible' role='alert'>${msg}</div>`; 
+}
+
+function validateEmail(value) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(value);
+}
+
 function validate(event){
     event.preventDefault()
     let email = document.getElementById("email");
     let password = document.getElementById("password");
-    
+
     if(password.value.length <1) {
-        alert("El password no puede estar vacío");
-        return false;
-    }
-    
-    if(email.value.length <1) {
-        alert("El email debe contener caracteres");
+        setAlert("El password debe contener caracteres!");
         return false;
     }
     
     if(email.value !== "jap@gmail.com" || password.value !=="123") {
-        alert("Los datos ingresados no son correctos");
+        setAlert("Las credenciales no son validas!");
         return false;
     }
-    window.location.replace("listing.html");
-
+    window.location.replace("index.html");
 }
+
 
 form.addEventListener("submit",validate);
---
+++ js/products.js
@@ -0,0 +1,49 @@
+
+function buildCard(title, currency, price, image, description, sold) {
+    return `
+    <div class="row justify-content-center mt-3 mb-1">
+        <div class="col-md-12">
+            <div class="card shadow-0 border rounded-3">
+                <div class="card-body product">
+                    <div class="row">
+                        <div class="col-md-12 col-lg-3 col-xl-3 mb-4 mb-lg-0">
+                            <div class="bg-image hover-zoom ripple rounded ripple-surface">
+                                <img src="${image}" class="w-100">
+                                <a href="#!">
+                                <div class="hover-overlay">
+                                    <div class="mask" style="background-color: rgba(253, 253, 253, 0.15);"></div>
+                                </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-8 col-lg-8 col-xl-8">
+                            <h5>${title} - ${currency} ${price}</h5>
+                            <p class="text-truncate mb-4 mb-md-0">
+                                ${description}
+                            </p>
+                        </div>
+                        <div class="col-md-1 col-lg-1 col-xl-1 border-sm-start-none">
+                            <div class="d-flex flex-row align-items-left mb-1">
+                                <h4 class="mb-1 me-1">${sold}</h4>
+                                <p class="small">vendidos</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+}
+
+function buildProducts(products) {
+    products.forEach(product => {
+        document.getElementById("products-container").innerHTML+= buildCard(product.name, product.currency, product.cost, product.image, product.description, product.soldCount);
+    });
+}
+
+fetch("https://japceibal.github.io/emercado-api/cats_products/101.json")
+.then(response => response.json())
+.then(function(data) {
+    document.getElementById("category-title").innerText=data.catName;
+    buildProducts(data.products);
+});
